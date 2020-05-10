13ea248cc7e4d282c9c130608395bcfbe0c5cf69
Ivani <katarzyna.stosio@gmail.com>
2020-05-10 08:50:26 +0200

    created required attribute

--- client/src/components/RegisterAccount/RegisterAccount.js
+++ client/src/components/RegisterAccount/RegisterAccount.js
@@ -21,29 +21,29 @@ export function RegisterAccount(){
             {console.log(errors)}
             <label className="firstName">
                 Imię:
-                <input type="text" id="firstName" value={formData.firstName} onChange={onChangeHandler}></input>
+                <input type="text" id="firstName" value={formData.firstName} onChange={onChangeHandler} required></input>
             </label>
             <label className="lastName">
                 Nazwisko:
-                <input type="text" id="lastName" value={formData.lastName} onChange={onChangeHandler}></input>
+                <input type="text" id="lastName" value={formData.lastName} onChange={onChangeHandler} required></input>
             </label>
             <label className="birthDate">
                 Data urodzenia:
                 <input type="date" max="2004-12-31" min="1900-01-01"
-                id="date" value={formData.date} onChange={onBirthDateChangeHandler} onKeyPress={onBirthDateChangeHandler}></input>
+                id="date" value={formData.date} onChange={onBirthDateChangeHandler} onKeyPress={onBirthDateChangeHandler} required></input>
                 {errors.date && <ErrorText error={errors.date} />}
             </label>
             <label className="email">
                 e-mail:
-                <input type="email" id="mail" value={formData.mail} onChange={onChangeHandler}></input>
+                <input type="email" id="mail" value={formData.mail} onChange={onChangeHandler} required></input>
             </label>
             <label className="password">
                 Hasło:
-                <input type="password" id="originalPassword" value={formData.originalPassword} onChange={onChangeHandler}></input>
+                <input type="password" id="originalPassword" value={formData.originalPassword} onChange={onChangeHandler} required></input>
             </label>
             <label className="repeatPassword">
                 Powtórz hasło:
-                <input type="password" id="repeatedPassword" onBlur={inputRepeatedPassword} value={formData.repeatedPassword} onChange={onChangeHandler}></input>
+                <input type="password" id="repeatedPassword" onBlur={inputRepeatedPassword} value={formData.repeatedPassword} onChange={onChangeHandler} required></input>
                 {!isPasswordValid && <ErrorText />}
             </label>
             <div className="createAccountButton">
