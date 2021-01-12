5331c85e5d124c2b6eb50aebf7be57a483eaeb71
Ivani <katarzyna.stosio@gmail.com>
2021-01-12 08:59:32 +0100

    last changes in MyProfile component

--- client/src/components/MyProfile/MyProfile.js
+++ client/src/components/MyProfile/MyProfile.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import {useMyProfileHooks} from './myProfileHooks'
+import React, {Component} from 'react';
+/*import {useMyProfileHooks} from './myProfileHooks'*/
+import {hobby} from '../RegisterProfile/dictionary';
 
 
-export function MyProfile () {
+/*export function MyProfile () {
 
     
 const {data, hobbys, age} = useMyProfileHooks()
@@ -30,6 +31,70 @@ const {data, hobbys, age} = useMyProfileHooks()
             </div>
         </div>
     );   
+}*/
+
+class MyProfile extends Component {
+    state = {data: null, error: null, accountData: null, hobbys: [], age: 0}
+    componentDidMount(){
+        fetch('/api/user')
+                .then(response => response.json())
+                .then(data => {
+                    this.setState({accountData: data, error: null})
+                })
+                .catch(error => {
+                    this.setState({accountData: null, error})
+                    console.error(error);
+                })
+        fetch('/api/user/details')
+                .then(response => response.json())
+                .then(data => {
+                    this.setState({data, error: null})
+                })
+                .catch(error => {
+                    this.setState({data: null, error})
+                    console.error(error);
+                })
+    }
+    componentDidUpdate(prevProps, prevState, snapshot){
+        if(this.state.data !== prevState.data){
+            const chosenHobbys = this.state.data?.interests;
+            const hobbys = hobby.filter((element) => chosenHobbys.includes(element.id)).map(el => el?.label)
+            this.setState({hobbys})
+        }
+        if(this.state.accountData !== prevState.accountData){
+            const now = new Date();
+            const birthDate = new Date(this.state.accountData.birth_date)
+            this.setState({age: now.getFullYear() - birthDate.getFullYear()})
+        }
+    }
+    render() {
+        return (
+            <div className="myProfile">
+            <div className="mainPicture">
+                
+            </div>
+            <div className="statusNickname">
+                <div className="nickname">
+                    {this.state.data?.nickname}
+                </div>
+                <div className="status">
+                    <img src={'/' + this.state.data?.relationship + '.png'} className="statusImg"></img>
+                </div>
+            </div>
+            <div className="interests">               
+                <div className="city">{this.state.data?.city}</div>
+                <div className="age">{this.state.age}</div>
+                <div className="hobbys">{this.state.hobbys.join(', ')}</div>
+            </div>
+            <div className="myPage">
+                
+            </div>
+        </div>
+        )
+    }
 }
 
 export default MyProfile;
+
+
+
