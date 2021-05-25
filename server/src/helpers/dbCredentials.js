a93010e983d6d6c88fc21c00287cab04064a8509
Ivani <katarzyna.stosio@gmail.com>
2021-05-25 16:33:38 +0200

    changes in dbCredentials

--- server/src/helpers/dbCredentials.js
+++ server/src/helpers/dbCredentials.js
@@ -2,7 +2,6 @@ module.exports = {
     host     : 'sql.mayanetbis.nazwa.pl',
     port     : 3306,
     user     : 'mayanetbis_testdb',
-    password : 'TESTdb123',
     database : 'mayanetbis_testdb'
   };
   
