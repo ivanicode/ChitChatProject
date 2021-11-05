33dff49c54599ed7df2e07c4f3c84142e89a6371
ivanicode <47351901+ivanicode@users.noreply.github.com>
2021-11-05 10:44:36 +0100

    Delete dbCredentials.js

--- server/src/helpers/dbCredentials.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    host     : 'sql.mayanetbis.nazwa.pl',
-    port     : 3306,
-    user     : 'mayanetbis_testdb',
-    password : 'TESTdb123',
-    database : 'mayanetbis_testdb'
-  };
-  
